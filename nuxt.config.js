d1e4f3026d9af341ad42a904608888c490e4fa43
janmi <627943558@qq.com>
2018-10-26 23:24:05 +0800

    feat: add nuxt config

---
+++ nuxt.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+	head: {
+		title: '众筹 DApp',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1'},
+			{ hid: 'description', name: 'description', content: '基于以太坊智能合约开发的众筹 DApp' },\
+		]
+	},
+	css: [{ src: 'element-ui/lib/theme-chalk/index.css' }],
+	plugins: [
+		{
+			src: '~plugins/element-ui',
+			ssr: true
+		}
+	],
+	build: {
+		extractCSS: true,
+		babel: {
+			plugins: [
+				[
+					"component",
+					{
+            "libraryName": "element-ui",
+            "styleLibraryName": "theme-chalk"
+					}
+				]
+			]
+		},
+		vendor: ['element-ui']
+	},
+};
