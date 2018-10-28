c0ca18b758bbe57623892f83877299a303b99437
janmi <627943558@qq.com>
2018-10-28 16:21:49 +0800

    feat: add default config

---
+++ config/default.js
@@ -0,0 +1,4 @@
+module.export = {
+	infuraUrl: 'https://rinkeby.infura.io/v3/8d6ae3c5f791485784fa1b9e3d306440',
+	hdwallet: 'exact worry once frog husband flavor silent cave alcohol innocent arrow purity'
+}
